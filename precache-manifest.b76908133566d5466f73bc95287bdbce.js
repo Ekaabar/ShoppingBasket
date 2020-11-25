69e7de7879d4b1c5b8a330882bfc155d99b4e3ce
Ekaabar <kaabar.eya@gmail.com>
2020-11-25 22:15:17 +0100

    Updates

---
+++ precache-manifest.b76908133566d5466f73bc95287bdbce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30639c982c892f266405",
+    "url": "/ShoppingBasket/static/css/main.52e21d72.chunk.css"
+  },
+  {
+    "revision": "30639c982c892f266405",
+    "url": "/ShoppingBasket/static/js/main.30639c98.chunk.js"
+  },
+  {
+    "revision": "56401b86207ef783b89e",
+    "url": "/ShoppingBasket/static/js/runtime~main.56401b86.js"
+  },
+  {
+    "revision": "c5c74e4f0e67ab4f0a20",
+    "url": "/ShoppingBasket/static/css/2.c0efc6d5.chunk.css"
+  },
+  {
+    "revision": "c5c74e4f0e67ab4f0a20",
+    "url": "/ShoppingBasket/static/js/2.c5c74e4f.chunk.js"
+  },
+  {
+    "revision": "c1758e1edef9de77fbc6f9ec8f58b227",
+    "url": "/ShoppingBasket/index.html"
+  }
+];
